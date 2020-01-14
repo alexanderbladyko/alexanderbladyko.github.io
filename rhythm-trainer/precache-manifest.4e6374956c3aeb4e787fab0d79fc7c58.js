8d09feea9c5bcc5a42b3491df4db0e2f2ed530a6
Aleksandr Bladyko <a_bladyko@wargaming.net>
2020-01-14 11:46:08 +0300

    rebuild pages

---
+++ rhythm-trainer/precache-manifest.4e6374956c3aeb4e787fab0d79fc7c58.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3a1978b4fe3382e886265b2b83ad355",
+    "url": "/rhythm-trainer/index.html"
+  },
+  {
+    "revision": "3d36ee98457f4b5f9b06",
+    "url": "/rhythm-trainer/static/css/main.093a6eb3.chunk.css"
+  },
+  {
+    "revision": "cc97a8a8203fb7a4634c",
+    "url": "/rhythm-trainer/static/js/2.3a91f1b8.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/rhythm-trainer/static/js/2.3a91f1b8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3d36ee98457f4b5f9b06",
+    "url": "/rhythm-trainer/static/js/main.f69f2aa7.chunk.js"
+  },
+  {
+    "revision": "7043d76f838269e264ea",
+    "url": "/rhythm-trainer/static/js/runtime-main.2cf90995.js"
+  },
+  {
+    "revision": "02d5c1919cfc6bbb3e8e0eebac5c67d7",
+    "url": "/rhythm-trainer/static/media/cancel.02d5c191.svg"
+  },
+  {
+    "revision": "31053e72490544d5be0f3f08e785188c",
+    "url": "/rhythm-trainer/static/media/eights.31053e72.svg"
+  },
+  {
+    "revision": "bb8e6d2832dba0d554b5fed0a8ceb6b1",
+    "url": "/rhythm-trainer/static/media/quarter.bb8e6d28.svg"
+  },
+  {
+    "revision": "1f967ff7810266a21f08671ed4c58731",
+    "url": "/rhythm-trainer/static/media/sixteens.1f967ff7.svg"
+  },
+  {
+    "revision": "e0d4aa7c6f4a22459f31e43aba97de5a",
+    "url": "/rhythm-trainer/static/media/triplet.e0d4aa7c.svg"
+  }
+]);
--- rhythm-trainer/service-worker.js
+++ rhythm-trainer/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c86a31fe8026ed07fa751f26f9b1c626.js"
+  "/rhythm-trainer/precache-manifest.4e6374956c3aeb4e787fab0d79fc7c58.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rhythm-trainer/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
